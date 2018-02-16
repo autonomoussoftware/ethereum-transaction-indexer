6c290dc8a49a88e953c403403ee0b685b201fe45
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-02-16 16:22:03 -0300

    Add restify-router and root endpoint

--- src/api.js
+++ src/api.js
@@ -1,12 +1,8 @@
 const { apiPort } = require('config')
 const restify = require('restify')
 const debug = require('debug')('eis.api')
-const promiseAllProps = require('promise-all-props')
 
-const merge = require('../lib/merge')
-
-const db = require('./db')
-
+const routes = require('./routes')
 const server = restify.createServer()
 
 server.use(restify.plugins.queryParser())
@@ -18,61 +14,9 @@ function logRequest (req, res, next) {
 
 server.use(logRequest)
 
-function getAddressTransactions (req, res, next) {
-  const address = req.params.address.toLowerCase()
-  const { from: min = 0, to } = req.query
-  return Promise.resolve(to || db.get('best-block'))
-    .then(function (max) {
-      return db.zrangebyscore(`eth:${address}`, min, max)
-        .then(function (transactions) {
-          debug('<-- eth', address, min, max, transactions.length)
-          res.json(transactions)
-          next()
-        })
-    })
-}
-
-server.get('/addresses/:address/transactions', getAddressTransactions)
-
-function getAddressTokenTransactions (req, res, next) {
-  const address = req.params.address.toLowerCase()
-  const { from: min = 0, to, tokens } = req.query
-
-  return promiseAllProps({
-    max: to || db.get('best-block'),
-    sets: tokens
-      ? tokens.split(',').map(t => `tok:${address}:${t}`)
-      : db.keys(`tok:${address}:*`)
-  })
-    .then(function ({ max, sets }) {
-      return Promise.all(sets.map(function (set) {
-        const token = set.substr(-42)
-        return db.zrangebyscore(set, min, max)
-          .then(transactions => ({ [token]: transactions }))
-      }))
-        .then(merge)
-        .then(function (transactions) {
-          debug('<-- tok', address, min, max, Object.keys(transactions).length)
-          res.json(transactions)
-          next()
-        })
-    })
-}
-
-server.get('/addresses/:address/tokentransactions', getAddressTokenTransactions)
-
-function getBestBlockNumber (req, res, next) {
-  db.get('best-block')
-    .then(function (number) {
-      console.log('<--', number)
-      res.json({ number })
-      next()
-    })
-}
-
-server.get('/blocks/latest/number', getBestBlockNumber)
-
 function start () {
+  routes.applyRoutes(server)
+
   server.listen(apiPort, function () {
     console.log(`API started on port ${apiPort}`)
   })
