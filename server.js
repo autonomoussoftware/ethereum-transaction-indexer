402d3f0355d417e21b273b433e6a43ef5fa44f99
Gabriel Montes <gabriel@bloq.com>
2018-04-05 17:15:37 -0300

    Added New Relic integration to api/indexer

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
 'use strict'
 
-const config = require('config')
-
-if (config.newRelic && config.newRelic.licenseKey) {
-  require('newrelic')
-}
-
-const indexer = require('./src/indexer')
-const api = require('./src/api')
-
-api.start()
-indexer.start()
+require('./bin/indexer')
+require('./bin/api')
