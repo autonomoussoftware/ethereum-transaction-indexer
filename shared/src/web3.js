add4b3a16f2b29b629f4e780c1ff297ac5253af9
Gabriel Montes <gabriel@bloq.com>
2019-04-24 18:31:47 -0300

    Default to WS and overwrite if IPC is set

--- shared/src/web3.js
+++ shared/src/web3.js
@@ -6,9 +6,9 @@ const Web3 = require('web3')
 function getWeb3 (config) {
   const { enode: { ipcPath, wsPath } } = config
 
-  const provider = ipcPath
-    ? new Web3.providers.IpcProvider(ipcPath, net)
-    : new Web3.providers.WebsocketProvider(wsPath, { autoReconnect: true })
+  const provider = wsPath
+    ? new Web3.providers.WebsocketProvider(wsPath, { autoReconnect: true })
+    : new Web3.providers.IpcProvider(ipcPath, net)
 
   const web3 = new Web3(provider)
 
